dec8b918b7c5551b4dc12283921cc2d730d46628
Viktar Kaniushyk <pishop.j@gmail.com>
2018-10-28 23:11:25 +0300

    fiz queue

--- src/Queue.js
+++ src/Queue.js
@@ -1,17 +1,16 @@
 export class Queue {
     messages = {};
 
-    addMessage(fn, chatId, param) {
+    addMessage = (fn, chatId, param) => {
         if (this.messages[chatId]) {
-            this.messages[chatId].push({ fn, chatId, param });
+            this.messages[chatId].push(() => fn(chatId, param));
         } else {
-            this.messages[chatId] = [{ fn, chatId, param }];
+            this.messages[chatId] = [() => fn(chatId, param)];
         }
 
         const interval = setInterval(() => {
             if (this.messages[chatId] && this.messages[chatId].length > 0) {
-                const { fn, chatId, param } = this.messages[chatId][0];
-                fn(chatId, param);
+                this.messages[chatId][0]();
                 this.messages[chatId] = this.messages[chatId].slice(1);
             } else {
                 clearInterval(interval);
